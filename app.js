28104efae19fc40241bd5c5be53d4ff58ddb77a1
Sujan Sapkota <Sujansapkota7777@gmail.com>
2022-11-27 20:59:26 +0545

    layout file fix

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const indexRoute = require('./routes/index')
-const userRoute = require('./routes/users')
-const path = require('path')
-const app = express()
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const indexRoute = require("./routes/index");
+const userRoute = require("./routes/users");
+const path = require("path");
+const app = express();
 
-PORT = process.env.PORT || 404
+PORT = process.env.PORT || 404;
 
 //EJS
-app.use(expressLayouts)
+app.use(expressLayouts);
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set('view engine','ejs')
 
 //Routes
-app.use('/',indexRoute)
-app.use('/user',userRoute)
+app.use("/", indexRoute);
+app.use("/user", userRoute);
 
-app.listen(PORT,()=>{
-    console.log(`Connected to port ${PORT}`);
-
-})
+app.listen(PORT, () => {
+  console.log(`Connected to port ${PORT}`);
+});
